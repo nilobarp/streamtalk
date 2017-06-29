0c5f686f76cd69f10774be303ed33544803a7fc6
Nilotpal Barpujari <nilotpal.barpujari@live.com>
2017-06-30 00:05:12 +0800

    use database config defined in app/config

--- test/init.test.ts
+++ test/init.test.ts
@@ -2,17 +2,10 @@ import * as mocha from 'mocha';
 import * as path from 'path';
 import { Constants } from '../core/string.constants';
 import { loadEnvironment, IOC } from '../core';
-import { DatabaseConfig } from '../core/types';
+import { DatabaseConfig as dbConfigType } from '../core/types';
+import { DatabaseConfig } from '../config/database';
 
 loadEnvironment(path.join(__dirname, '..', '..'));
 process.env[Constants.VAR_ROOT_PATH] = path.join(__dirname, 'artefacts', 'tmp');
 
-class MockDbConfig implements DatabaseConfig {
-    dialect = process.env.DB_DIALECT;
-    database = process.env.PGDATABASE;
-    user = process.env.PGUSER;
-    password = process.env.PGPASSWORD;
-    storage = path.resolve(__dirname, '..', '..', 'storage', 'db', 'app.sqlite');
-}
-
-IOC.Container.bind(DatabaseConfig).to(MockDbConfig).scope(IOC.Scope.Local);
+IOC.Container.bind(dbConfigType).to(DatabaseConfig).scope(IOC.Scope.Local);
